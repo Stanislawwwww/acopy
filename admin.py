a2df2aecc21c0ed2a1253e6ca3f45516579f1152
Stanislawwwww <antoska123456xaxa@gmail.com>
2024-06-24 08:11:18 +0300

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from goods.models import Categories, Products
+
+# admin.site.register(Categories)
+# admin.site.register(Products)
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name",]
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "category", "slug"]
+    list_editable = ["category"]
+    search_fields = ["name", "description"]
+    list_filter = ["name", "slug", "category"]
+    fields = [
+        "name",
+        "category",
+        "slug",
+        "description",
+    ]
